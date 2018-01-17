669e1a169326adca18b70008c4a796034ff12d97
Jay Fleming <jacobmfleming@gmail.com>
2018-01-17 21:53:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
 
   def position_taken?(location)
     @location = location
-    @board[location] != " " && board[location] != ""
+    @board[location] != " " && @board[location] != ""
   end
 
   def valid_move?(board, index)
