2cae281f2275895cb4d106dc31d8cdeafe52d8b9
Jay Fleming <jacobmfleming@gmail.com>
2018-01-17 21:55:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,4 +72,16 @@ class TicTacToe
     end
   end
 
+  def turn
+    puts "Please enter 1-9:"
+    index = input_to_index(gets.chomp)
+    current_player = current_player(board)
+    if valid_move?(board, index) == true
+      move(board, index, current_player)
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+
 end
